2a71512e8e56784e6a86a1c47209d9c003811ffa
molotoM <75744188+molotoM@users.noreply.github.com>
2021-01-26 04:03:02 +0200

    Add files via upload

---
+++ onlinePharmacy/onlinePharmacy/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace onlinePharmacy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbSymptoms = new System.Windows.Forms.ComboBox();
+            this.lstSymptoms = new System.Windows.Forms.ListBox();
+            this.submitSymptoms = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblMedicine = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(2, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PHARMACY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(58, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "at your finger tips";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(2, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select your symptoms below";
+            // 
+            // cmbSymptoms
+            // 
+            this.cmbSymptoms.FormattingEnabled = true;
+            this.cmbSymptoms.Location = new System.Drawing.Point(2, 134);
+            this.cmbSymptoms.Name = "cmbSymptoms";
+            this.cmbSymptoms.Size = new System.Drawing.Size(197, 23);
+            this.cmbSymptoms.TabIndex = 3;
+            this.cmbSymptoms.SelectedIndexChanged += new System.EventHandler(this.cmbSymptoms_SelectedIndexChanged);
+            // 
+            // lstSymptoms
+            // 
+            this.lstSymptoms.FormattingEnabled = true;
+            this.lstSymptoms.ItemHeight = 15;
+            this.lstSymptoms.Location = new System.Drawing.Point(205, 115);
+            this.lstSymptoms.Name = "lstSymptoms";
+            this.lstSymptoms.Size = new System.Drawing.Size(144, 94);
+            this.lstSymptoms.TabIndex = 4;
+            this.lstSymptoms.SelectedIndexChanged += new System.EventHandler(this.lstSymptoms_SelectedIndexChanged);
+            // 
+            // submitSymptoms
+            // 
+            this.submitSymptoms.BackColor = System.Drawing.Color.Red;
+            this.submitSymptoms.ForeColor = System.Drawing.Color.White;
+            this.submitSymptoms.Location = new System.Drawing.Point(124, 179);
+            this.submitSymptoms.Name = "submitSymptoms";
+            this.submitSymptoms.Size = new System.Drawing.Size(75, 30);
+            this.submitSymptoms.TabIndex = 5;
+            this.submitSymptoms.Text = "Submit";
+            this.submitSymptoms.UseVisualStyleBackColor = false;
+            this.submitSymptoms.Click += new System.EventHandler(this.submitSymptoms_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblDescription.ForeColor = System.Drawing.Color.White;
+            this.lblDescription.Location = new System.Drawing.Point(2, 235);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(161, 21);
+            this.lblDescription.TabIndex = 7;
+            this.lblDescription.Text = "write your description";
+            // 
+            // lblMedicine
+            // 
+            this.lblMedicine.AutoSize = true;
+            this.lblMedicine.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblMedicine.ForeColor = System.Drawing.Color.White;
+            this.lblMedicine.Location = new System.Drawing.Point(2, 296);
+            this.lblMedicine.Name = "lblMedicine";
+            this.lblMedicine.Size = new System.Drawing.Size(161, 21);
+            this.lblMedicine.TabIndex = 7;
+            this.lblMedicine.Text = "write your description";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Red;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(1, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(293, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Do you want us to recomend a Doctor for you?";
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(300, 328);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(35, 23);
+            this.btnYes.TabIndex = 9;
+            this.btnYes.Text = "YES";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(331, 328);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(35, 23);
+            this.btnNo.TabIndex = 9;
+            this.btnNo.Text = "NO";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(367, 353);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMedicine);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.submitSymptoms);
+            this.Controls.Add(this.lstSymptoms);
+            this.Controls.Add(this.cmbSymptoms);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbSymptoms;
+        private System.Windows.Forms.ListBox lstSymptoms;
+        private System.Windows.Forms.Button submitSymptoms;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblMedicine;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ onlinePharmacy/onlinePharmacy/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace onlinePharmacy
+{
+    public partial class Form1 : Form
+    {
+
+        int numberOfSelectedSymptoms = 0;
+        int indexOfHighest = 0;
+        List<string> symptomsList = new List<string>();
+        List<string> dataList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            lblDescription.Visible = false;
+            lblMedicine.Visible = false;
+            label5.Visible = false;
+            btnNo.Visible = false;
+            btnYes.Visible = false;
+
+
+            //READ FROM FILE==================================================
+            using (var reader = new StreamReader("MOCK_DATA.csv"))
+                
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    string oneLine = reader.ReadLine();
+                    int numberOfSymptomps = 0;
+                    string findAllSyptomps = findSymptomps(oneLine, 4);
+
+                    dataList.Add(oneLine);
+                    symptomsList.Add(findAllSyptomps);
+                    
+                    
+                    //FIND SYMPTOMS IN ONE lINE======================
+                    for (int x = 0; x < findAllSyptomps.Length; x++)
+                    {
+                        if (findAllSyptomps.Substring(x, 1) == ".")
+                        {
+                            numberOfSymptomps++;
+                        }
+                    }
+
+                    //ADD SYMPTOMS IN A COMBO BOX===========================
+                    for (int x = 0; x < numberOfSymptomps; x++)
+                    {
+                        cmbSymptoms.Items.Add(findAllSyptomps.Substring(0, findAllSyptomps.IndexOf(".")));
+                        findAllSyptomps = findAllSyptomps.Remove(0, findAllSyptomps.IndexOf(".") + 1);
+                    }
+                }
+
+            }
+            
+        }
+
+        String findSymptomps(string dataLine, int columnNumber)
+        {
+            string wholeString = dataLine + ",hjfhgf,", newWord = "";
+
+            for (int x = 0; x < columnNumber; x++)
+            {
+                newWord = dataLine.Substring(0, dataLine.IndexOf(','));
+                dataLine = dataLine.Remove(0, dataLine.IndexOf(',') + 1);
+
+            }
+            return newWord;
+        }
+        private void cmbSymptoms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbSymptoms.SelectedItem!=null )
+            {
+                string symptom = cmbSymptoms.SelectedItem.ToString();
+                bool alreadySelected = false;
+                numberOfSelectedSymptoms = lstSymptoms.Items.Count;
+
+                for (int x = 0; x < numberOfSelectedSymptoms; x++) {
+                    if (symptom == lstSymptoms.Items[x].ToString())
+                    {
+                        alreadySelected = true;
+                        
+                    }
+                }
+                if(!alreadySelected)
+                    lstSymptoms.Items.Add(symptom);
+
+            }
+        }
+
+        private void lstSymptoms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lstSymptoms.SelectedItem!= null)
+            {
+                int removeSymptom = lstSymptoms.SelectedIndex;
+
+                lstSymptoms.Items.RemoveAt(removeSymptom);
+
+            }
+        }
+
+        private void submitSymptoms_Click(object sender, EventArgs e)
+        {
+
+            numberOfSelectedSymptoms = lstSymptoms.Items.Count;
+            int highestMatch = 0;
+
+            List<int> matchInARow = new List<int>();
+
+            for (int x = 0; x < numberOfSelectedSymptoms; x++)
+            {
+
+                foreach (string value in symptomsList)
+                {
+                    int currentMatch = 0;
+                    int symptompsIn1Line = 0;
+                    string i = value + ".";
+
+                    for (int y = 0; y < i.Length; y++)
+                    {
+
+                        if (i.Substring(y, 1) == ".")
+                        {
+                            symptompsIn1Line++;
+
+                        }
+                    }
+
+                    for (int y = 0; y < symptompsIn1Line; y++)
+                    {
+                        int theIndex = i.IndexOf('.');
+
+
+                        if (lstSymptoms.Items[x].ToString() == i.Substring(0, theIndex))
+                        {
+                            currentMatch++;
+
+                        }
+                        if (y != symptompsIn1Line - 2)
+                            i = i.Remove(0, theIndex + 1);
+
+
+                    }
+                    matchInARow.Add(currentMatch);
+
+
+                }
+
+            }
+            //FIND THE ROW WITH THE HIGHEST MATCH===========================================
+            foreach (int count in matchInARow)
+            {
+                if (count > highestMatch)
+                {
+                    highestMatch = count;
+                    indexOfHighest = matchInARow.IndexOf(count);
+
+                }
+
+            }
+
+            //SHOW THE ILLNESS ASSOCIATED WITH HIGHEST SYMPTOMS=================================
+            String message1 = "You have syptoms of ", message2 = " Can we recommend medicine for you?";
+            String disease = findSymptomps(dataList[indexOfHighest], 2);
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message1 + disease + Environment.NewLine + message2, "OnlinePharmacy", buttons);
+            if (result == DialogResult.Yes)
+            {
+                lblMedicine.Visible = true;
+                lblDescription.Visible = true;
+                label5.Visible = true;
+                btnNo.Visible = true;
+                btnYes.Visible = true;
+
+                lblDescription.Text = disease + Environment.NewLine + findSymptomps(dataList[indexOfHighest], 3);
+                lblMedicine.Text = "You can use " + findSymptomps(dataList[indexOfHighest].Replace(" ", string.Empty), 5)+" to treat it";
+
+            }
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can contact " + findSymptomps(dataList[indexOfHighest], 6) + Environment.NewLine +
+                findSymptomps(dataList[indexOfHighest], 7)) ;//+ Environment.NewLine + findSymptomps(dataList[indexOfHighest], 8)) ;
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            lblDescription.Visible = false;
+            lblMedicine.Visible = false;
+            label5.Visible = false;
+            btnNo.Visible = false;
+            btnYes.Visible = false;
+        }
+    }
+}
